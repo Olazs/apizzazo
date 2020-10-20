03301e3bf76a0d7782b00dd6a310632403139f5a
Olazs <warplanes96@hotmail.com>
2020-10-20 13:19:08 +0200

    Meg a javascript nem mukodik

---
+++ main.js
@@ -0,0 +1,24 @@
+var xd=0;
+
+function kosar(num){
+    if (num==1){
+        xd+=1490;
+   }
+   if(num==2){
+    xd+=1590;
+   }
+   if(num==3){
+    xd+=1690;  
+}
+if(num==4){
+    xd+=1790; 
+}
+if(num==5){
+    xd+=1890; 
+}
+if(num==6){
+    xd+=1990;   
+}
+alert(xd);
+}
+alert(xd);
